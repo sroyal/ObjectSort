9734a687f6d421384b139fa6a47e821c3ca5cb2a
sroyal <se.royal@gmail.com>
2014-08-21 12:38:53 -0500

    ObjectSort
    Creates an array of objects, generates random numbers, stores them in
    the array.  Prints out the unsorted array. Then sorts the array, and
    prints out the sorted array.

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectSort
+{
+    class Program
+    {
+        string storeName;
+        static void Main(string[] args)
+        {
+            
+            
+            Store st129 = new Store();
+            //Create an array of store objects
+            Store[] storeArray = new Store[10];
+            for (int i = 0; i < 10; i++)
+            {
+               storeArray[i] = new Store();
+            }
+
+            //random number generator
+             Random rand = new Random();
+            
+            //stores a random number from 1 to 500
+            //in the store array
+            for (int i = 0; i < 10; i++)
+            {
+                storeArray[i].poNumber = rand.Next(1, 500);
+            }
+
+            //prints out the numbers in from array
+            foreach (Store value in storeArray)
+            {
+                Console.WriteLine(value.poNumber);
+            }
+
+            Console.WriteLine();
+            //Sorts the array
+            Array.Sort(storeArray);
+            //Prints out the sorted Array
+            foreach (Store value in storeArray)
+            {
+                Console.WriteLine(value.poNumber);
+            }
+
+
+            
+
+           
+            //storeArray[0] = st129;
+            //storeArray[1] = new Store();
+            //storeArray[2] = new Store();
+            //storeArray[1].poNumber = 100;
+            //storeArray[2].poNumber = 200;
+            //Console.WriteLine(storeArray[1].poNumber);
+            //Console.WriteLine(storeArray[2].poNumber);
+            //storeArray[1].poNumber = 2;
+            //List<Store> StoreList = new List<Store>();
+            //StoreList.Add(new Store { });
+            Console.Read();
+            
+        }
+    }
+}
---
+++ store.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectSort 
+{
+    class Store : IComparable
+    {
+        public int CompareTo(object obj)
+        {
+            if (obj is Store)
+            {
+                return this.poNumber.CompareTo((obj as Store).poNumber);  // compare numbers
+            }
+            throw new ArgumentException("Object is not a User");
+        }
+        int _storeNumber; 
+        int _pONumber;
+
+        public int storeNumber
+        {
+            get
+            {
+                return _storeNumber;
+            }
+            set
+            {
+                _storeNumber = value;
+            }
+        }
+
+        public int poNumber
+        {
+            get
+            {
+                return _pONumber;
+            }
+            set
+            {
+                _pONumber = value;
+            }
+        }
+
+
+        
+    }
+}
